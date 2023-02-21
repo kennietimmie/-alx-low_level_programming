209d88ab4de134b0d9dbcdcf67a62b2fe7c8f843
Kehinde Sholadoye <kehinde.bridgewateracademy@gmail.com>
2023-02-21 01:03:03 +0100

    Create 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char c = 'a';
+
+	for (;c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
