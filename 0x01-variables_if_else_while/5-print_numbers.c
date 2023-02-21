2192b9cfe25fb10c673eca552864e2bb4c91f7e2
Kehinde Sholadoye <kehinde.bridgewateracademy@gmail.com>
2023-02-21 01:09:35 +0100

    Create 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+	int number = 0;
+
+	for (; number < 10; number++)
+		printf("%i", number);
+	putchar('\n');
+	return (0);
+}
