016c86f68a37e130193ebb3737b14b9faf8a50a4
Kehinde Sholadoye <kehinde.bridgewateracademy@gmail.com>
2023-02-21 01:08:01 +0100

    Create 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c != 'e' && c != 'q')
+			putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return
