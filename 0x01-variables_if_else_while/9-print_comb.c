449db994f05a0240d604ea118e02224768ac8d9a
Kehinde Sholadoye <kehinde.bridgewateracademy@gmail.com>
2023-02-21 01:24:15 +0100

    Create 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+	int number = '0';
+
+	for (; number <='9'; number++)
+	{
+		putchar(number);
+		if (number < '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
