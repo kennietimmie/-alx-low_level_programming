f0c780314dd274ebd7cc8ac33839d0039ddb7c07
Kehinde Sholadoye <kehinde.sholadoye@yahoo.com>
2023-02-25 17:45:21 +0100

    update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -5,33 +5,33 @@
  */
 int main(void)
 {
-  int a = 48, b, c, d;
-  
-  for (; a <= 57; a++)
-  {
-    for (b = 48; b <= 57; b++)
-    {
-      for (c = 48; c <= 57; c++)
-      {
-        for (d = 49; d <= 57; d++)
-        {
-          if (a + b < c + d)
-          {
-            putchar(a);
-            putchar(b);
-            
-            putchar(' ');
-            
-            putchar(c);
-            putchar(d);
-            
-            putchar(',');
-            putchar(' ');
-          }
-        }
-      }
-    }
-  }
-  putchar('\n');
-  return (0);
+	int a = 48, b, c, d;
+	
+	for (; a <= 57; a++)
+	{
+		for (b = 48; b <= 57; b++)
+		{
+			for (c = 48; c <= 57; c++)
+			{
+				for (d = 49; d <= 57; d++)
+				{
+					if (a + b < c + d)
+					{
+						putchar(a);
+						putchar(b);
+						
+						putchar(' ');
+						
+						putchar(c);
+						putchar(d);
+						
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
