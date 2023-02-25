cab6d182d49cb18adad6a29e4c5f669f6c7bf400
Kehinde Sholadoye <kehinde.sholadoye@yahoo.com>
2023-02-25 12:14:41 +0100

    betty style guide

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,25 +1,29 @@
 #include <stdio.h>
 
-int main(void){
+int main(void)
+{
+	int first = 48, second;
 
-  int first = 48, second;
-  
-  for(; first <= 57; first++){   
-    for(second = 49; second <= 57; second++){
+  	for(; first <= 57; first++)
+	{
+    		for(second = 49; second <= 57; second++)
+		{
 
-      if(first < second){
-        putchar(first);
-        putchar(second); 
-        
-        if(first < 56 || second < 56) {
-          putchar(',');
-          putchar(' ');
-        }
-      }
-      
-    } 
-    
-  }
-  putchar('\n');
-  return 0;
+      			if(first < second)
+			{
+        			putchar(first);
+        			putchar(second);
+
+        			if(first < 56 || second < 56)
+				{
+          				putchar(',');
+          				putchar(' ');
+        			}
+      			}
+
+    		}
+
+  	}
+	putchar('\n');
+	return 0;
 }
