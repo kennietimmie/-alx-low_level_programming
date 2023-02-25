01598af57b0cabffc57e62438a152bd3aaf36551
Kehinde Sholadoye <kehinde.sholadoye@yahoo.com>
2023-02-25 12:34:55 +0100

    fix betty style bug

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,28 +7,23 @@
 
 int main(void)
 {
-	int first = 48, second;
-
-  	for(; first <= 57; first++)
-	{
-    		for(second = 49; second <= 57; second++)
-		{
-
-      			if(first < second)
-			{
-        			putchar(first);
-        			putchar(second);
-
-        			if(first < 56 || second < 56)
-				{
-          				putchar(',');
-          				putchar(' ');
-        			}
-      			}
-
-    		}
-
-  	}
-	putchar('\n');
-	return 0;
+  int first = 48, second;
+  for(; first <= 57; first++)
+  {
+    for(second = 49; second <= 57; second++)
+    {
+      if(first < second)
+      {
+        putchar(first);
+        putchar(second);
+        if(first < 56 || second < 56)
+        {
+          putchar(',');
+          putchar(' ');
+        }
+      }
+    }
+  }
+  putchar('\n');
+  return 0;
 }
