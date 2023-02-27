c4236b26beec36e0f150b05a2675ceb57eb5417b
Kehinde Sholadoye <kehinde.sholadoye@yahoo.com>
2023-02-27 22:47:56 +0100

    update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,21 +1,4 @@
 #include "main.h"
-#include <stdio.h>
-
-/**
- * main - check the code
- *
- * Return: Always 0
- */
-int main(void)
-{
-	int n;
-
-	n = 402;
-	printf("n=%d\n", n);
-	reset_to_98(&n);
-	printf("n=%d\n", n);
-	return (0);
-}
 
 /**
  * reset - Set value to 98
