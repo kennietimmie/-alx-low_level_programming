74b295c8666b36c0c2160bfa0ac86d1d5c6bac9a
Kehinde Sholadoye <kehinde.sholadoye@yahoo.com>
2023-02-27 22:20:38 +0100

    pointer to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+void reset_to_98(int *n);
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+
+	n = 402;
+	printf("n=%d\n", n);
+	reset_to_98(&n);
+	printf("n=%d\n", n);
+	return (0);
+}
+
+/**
+ * reset - Set value to 98
+ *
+ * Return: Always void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
