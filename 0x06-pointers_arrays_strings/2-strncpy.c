123128db36589f88b795f2d050b9c3dcd3bbd8ff
Kehinde Sholadoye <kehinde.sholadoye@yahoo.com>
2023-03-01 21:02:04 +0100

    strncpy definition

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strncpy - Copy n numbers of string.
+ * @dest: Destination string
+ * @src: Source string.
+ * @n: number of string to copy.
+ *
+ * Return: Always char *
+ */
+char *_strcpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		if (*(src + i) == '\0')
+			*(dest + i) = '\0';
+
+		else
+			*(dest + i) = *(src + i);
+		i++;
+	}
+	return (dest);
+}
+
