d715ee2b0a93376b7f385cc85750b5bfdd7c8e6e
Kehinde Sholadoye <kehinde.bridgewateracademy@gmail.com>
2023-03-02 00:18:42 +0100

    Create 7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * leet - encode string.
+ * @s: string
+ *
+ * Return: pointer to string.
+ */
+char *leet(char *s)
+{
+	int i = 0;
+	
+	while (*(s + i))
+	{
+		switch (*(s + i))
+		{
+		case ('a'):
+		case ('A'):
+			*(s + i) = '4';
+			break;
+		case ('e'):
+		case ('E'):
+			*(s + i) = '3';
+			break;
+		case ('o'):
+		case ('O'):
+			*(s + i) = '0';
+			break;
+		case ('t'):
+		case ('T'):
+			*(s + i) = '7';
+			break;
+		case ('l'):
+		case ('L'):
+			*(s + i) = '1';
+			break;
+		}
+		i++;
+	}
+
+	return (s);
+}
