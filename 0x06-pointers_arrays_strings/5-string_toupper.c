b312de2e2e2b07e1740793dea7826958e738623e
Kehinde Sholadoye <kehinde.sholadoye@yahoo.com>
2023-03-01 22:52:48 +0100

    fix bug

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -12,9 +12,9 @@ char *string_toupper(char *s)
 
 	while (*(s + i))
 	{
-		if(*(str + i) >= 97 && *(str + i) <= 122)
+		if(*(s + i) >= 97 && *(s + i) <= 122)
 		{
-			*(str + i) = *(str + i) - 32;
+			*(s + i) = *(s + i) - 32;
 		}
 		i++;
 	}
