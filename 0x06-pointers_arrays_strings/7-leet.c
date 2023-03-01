82ae06f8f9ce1c71dac840c416ef817921c62380
Kehinde Sholadoye <kehinde.sholadoye@yahoo.com>
2023-03-02 00:24:30 +0100

    update 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -14,29 +14,30 @@ char *leet(char *s)
 	{
 		switch (*(s + i))
 		{
-		case ('a'):
-		case ('A'):
-			*(s + i) = '4';
-			break;
-		case ('e'):
-		case ('E'):
-			*(s + i) = '3';
-			break;
-		case ('o'):
-		case ('O'):
-			*(s + i) = '0';
-			break;
-		case ('t'):
-		case ('T'):
-			*(s + i) = '7';
-			break;
-		case ('l'):
-		case ('L'):
-			*(s + i) = '1';
-			break;
+			case ('a'):
+			case ('A'):
+				*(s + i) = '4';
+				break;
+			case ('e'):
+			case ('E'):
+				*(s + i) = '3';
+				break;
+			case ('o'):
+			case ('O'):
+				*(s + i) = '0';
+				break;
+			case ('t'):
+			case ('T'):
+				*(s + i) = '7';
+				break;
+			case ('l'):
+			case ('L'):
+				*(s + i) = '1';
+				break;
 		}
 		i++;
 	}
-
+	
 	return (s);
 }
+
