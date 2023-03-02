28437d66a68f098c895be08aa576fa7f3a620801
Kehinde Sholadoye <kehinde.bridgewateracademy@gmail.com>
2023-03-02 13:15:33 +0100

    Create 101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_number - prints an integer.
+ * @n: integer to be printed.
+ *
+ * Return: void.
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		putchar(45);
+		n = -n;
+	}
+	if (n / 10)
+		print_number(n / 10);
+	putchar(n % 10 + '0');
+}
