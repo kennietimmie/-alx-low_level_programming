199f4b4f060d21bea427c799b977b28f0d4e9ca1
Kehinde Sholadoye <kehinde.bridgewateracademy@gmail.com>
2023-03-02 03:01:27 +0100

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,7 +11,7 @@ int _strcmp(char *s1, char *s2)
 	int counter, cmp;
 
 	counter = 0;
-	while(s1[counter] == s2[counter] && s1[counter] != '\0'){
+	while (s1[counter] == s2[counter] && s1[counter] != '\0'){
 		counter++;
 	}
 	cmp = s1[counter] - s2[counter];
