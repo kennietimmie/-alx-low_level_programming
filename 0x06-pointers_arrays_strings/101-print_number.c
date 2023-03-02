e74b6e60c1a806988e0ce3b6400a2c8b4d9fdd94
Kehinde Sholadoye <kehinde.bridgewateracademy@gmail.com>
2023-03-02 13:23:06 +0100

    Update 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,16 +7,14 @@
  */
 void print_number(int n)
 {
-	int num = n;
-	
-	if (num < 0)
+	if (n < 0)
 	{
 		_putchar(45);
-		num = -n;
+		n = -n;
 	}
-	if (num / 10)
+	if (n / 10)
 	{
-		print_number(num / 10);
+		print_number(n / 10);
 	}
-	_putchar((num % 10) + '0');
+	_putchar((n % 10) + '0');
 }
