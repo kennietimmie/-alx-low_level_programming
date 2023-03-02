5daef0fbce1097b6e49e67a9ae580e9450bc4bd3
Kehinde Sholadoye <kehinde.bridgewateracademy@gmail.com>
2023-03-02 15:18:51 +0100

    Update 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -9,14 +9,12 @@ void print_number(int n)
 {
 	unsigned int num;
 
-	/*check if number is negative*/
 	num = n;
 	if (n < 0)
 	{
 		_putchar(45);
 		num = -n;
 	}
-	/* print number by recursion*/
 	if (num / 10)
 	{
 		print_number(num / 10);
