87019c584bb61f1a93576876d889231d958080b9
Kehinde Sholadoye <kehinde.bridgewateracademy@gmail.com>
2023-03-02 02:50:34 +0100

    Update 4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * reverse_array - reverse the content of an array
  * of integers.
@@ -10,14 +9,15 @@
  */
 void reverse_array(int *a, int n)
 {
-	int i = 0, tmp, j = n - 1;
+	int counter = 0, temp, limit = n - 1;
 
-	while (i != n / 2)
+	while (counter <= limit)
 	{
-		tmp = *(a + (j - i));
-		*(a + (j - i)) = *(a + i);
-		*(a + i) = tmp;
-		i++;
+		temp = a[limit];
+		a[limit] = a[counter];
+		a[counter] = temp;
+		counter++;
+		limit--;
 	}
 }
 
