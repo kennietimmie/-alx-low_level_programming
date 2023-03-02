525a6b98764c12f4b932d0da18be4df81d5d55ef
Kehinde Sholadoye <kehinde.sholadoye@yahoo.com>
2023-03-02 01:49:16 +0100

    update 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,16 +9,16 @@
 char *leet(char *s)
 {
 	int i = 0, j;
-	char lower[] = "aeotl", upper[] = "AEOTL";
-	int num[] = {52, 51, 48, 55, 49};
+	char decode[] = "aeotlAEOTL";
+	int encode[] = {52, 51, 48, 55, 49, 52, 51, 48, 55, 49};
 	
 	while (*(s + i))
 	{
 		for (j = 0; j < 5; j++)
 		{
-			if (*(s + i) == lower[j] || *(s + i) == upper[j])
+			if (*(s + i) == decode[j])
 			{
-				*(s + i) = num[j];
+				*(s + i) = encode[j];
 				break;
 			}
 		}
