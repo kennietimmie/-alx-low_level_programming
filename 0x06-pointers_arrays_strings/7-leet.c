ed61fceb79bde3dd6f85acbdc799ae097d77bfb2
Kehinde Sholadoye <kehinde.bridgewateracademy@gmail.com>
2023-03-02 02:33:57 +0100

    Update 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,32 +1,29 @@
 #include "main.h"
 /**
- * leet - encodes a string into 1337.
- * letters a and A are replaced by 4.
- * Letters e and E are replaced by 3
- * Letters o and O are replaced by 0
- * Letters t and T are replaced by 7
- * Letters l and L are replaced by 1
+ * leet - encodes a string.
  * @s: pointer to string.
  *
  * Return: pointer to s.
  */
 char *leet(char *s)
 {
-	int i = 0, j;
-	char decode[] = "aAeEoOtTlL";
-	char encode[] = "4433007711";
-	
-	while (s[i] != '\0')
+	int s_count, l_count;
+	char letters[] = "aAeEoOtTlL";
+	char nums[] = "4433007711";
+
+	s_count = 0;
+	while (s[s_count] != '\0')
 	{
-		for (j = 0; 10; j++)
+		l_count = 0;
+		while (l_count < 10)
 		{
-			if (s[i] == decode[j])
+			if (letters[l_count] == s[s_count])
 			{
-				s[i] = encode[j];
-				break;
+				s[s_count] = nums[l_count];
 			}
+			l_count++;
 		}
-		i++;
+		s_count++;
 	}
 	return (s);
 }
