b9a14c2793225e48ae2a8d6aa17887c93d42d402
Kehinde Sholadoye <kehinde.bridgewateracademy@gmail.com>
2023-03-07 01:22:22 +0100

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,13 +4,15 @@
  * @s: char pointer.
  * @b: character to fill.
  * @n: numbers character to fill.
+ *
  * Return: pointer to char.
  */
 char *_memset(char *s, char b, unsigned int n)
-{	
+{
 	unsigned int counter;
-
+	
 	for (counter = 0; counter < n; counter++)
 		s[counter] = b;
+	
 	return (s);
 }
