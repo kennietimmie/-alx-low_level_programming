18f36ecc5b6071d6d3019ca51a2103e80726cef2
Kehinde Sholadoye <kehinde.bridgewateracademy@gmail.com>
2023-03-07 01:24:57 +0100

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,10 +9,10 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int counter;
+	unsigned int i;
 	
-	for (counter = 0; counter < n; counter++)
-		s[counter] = b;
+	for (i = 0; i < n; i++)
+		s[i] = b;
 	
 	return (s);
 }
