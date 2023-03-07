381a7348a81f969c4bc780cbbebb85dd0943b861
Kehinde Sholadoye <kehinde.bridgewateracademy@gmail.com>
2023-03-07 01:12:11 +0100

    Update 7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -7,13 +7,12 @@
  */
 void print_chessboard(char (*a)[8])
 {
-	int i;
+	int i, j;
 
-	for (i = 0; i < 64; i++)
+	for (i = 0; i < 8; i++)
 	{
-		if (i % 8 == 0 && i != 0)
-			_putchar('\n');
-		_putchar(a[i / 8][i % 8]);
+		for (j = 0; j < 8; j++)
+			_putchar(a[i][j]);
+		_putchar('\n');		
 	}
-	_putchar('\n');
 }
