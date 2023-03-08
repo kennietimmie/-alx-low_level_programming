a5ad838176681dbed4ec777a408450c82f7ba3a4
Kehinde Sholadoye <kehinde.sholadoye@yahoo.com>
2023-03-08 23:30:45 +0100

    initialize 100-is_palindrome.c

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * is_palindrome - check if string is empty.
+ * @s: string.
+ *
+ * Return: 1 if string is empty otherwise 0.
+ */
+int is_palindrome(char *s)
+{
+	if (*s == '')
+		return (1);
+	return (0);
+}
+
