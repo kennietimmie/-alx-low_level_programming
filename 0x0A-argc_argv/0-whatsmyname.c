9ffdd84ecbb66e9f4bede13171ca933e56ababe0
Kehinde Sholadoye <kehinde.sholadoye@yahoo.com>
2023-03-10 20:59:26 +0100

    create 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check code
+ * @argc: numbers of argument passed
+ * @argv: pointer to arrays of passed arguments
+ *
+ * Return: Always 0;
+ */
+int main(int argc __attribute__ ((unused)), char *argv[])
+{
+	whatsmyname(argv[0]);
+	return (0);
+}
+
+/**
+ * whatsmyname - print program name
+ *
+ * Return: void.
+ */
+void whatsmyname(char *s)
+{
+	printf("%s\n", s);
+}
+
