bf29b25c849dd2f20b3b6e02fe2104e1fba886e8
Kehinde Sholadoye <kehinde.sholadoye@yahoo.com>
2023-03-10 17:19:46 +0100

    remove unused code

--- 0x09-static_libraries/9-strcpy.c
+++ 0x09-static_libraries/9-strcpy.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * _strcpy - Copy string from src to dest.
@@ -20,21 +19,5 @@ char *_strcpy(char *dest, char *src)
 
 	dest[i] = '\0';
 	return (dest);
-}
-
-/**
- * main - check code.
- *
- * Return: always 0.
- */
-int main(void)
-{
-	char s1[98];
-	char *ptr;
-
-	ptr = _strcpy(s1, "First, solve the problem. Then, write the code.\n");
-	printf("%s", s1);
-	printf("%s", ptr);
-	return (0);
 }
 
