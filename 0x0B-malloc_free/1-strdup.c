605d1305806fc4c86b219ae9f68bb0290846a26d
Kehinde Sholadoye <kehinde.sholadoye@yahoo.com>
2023-03-13 22:49:21 +0100

    initialize 1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - allocate space and copy string
+ * @str: string to copy.
+ *
+ * Return: if successful pointer to copied string otherwise NULL
+ */
+char *_strdup(char *str)
+{
+	char *cstr;
+	int i = 0;
+
+	if (str == NULL)
+		return (NULL);
+	while (*(str + i) != '\0')
+		i++;
+	cstr = malloc(sizeof(char) * --i);
+	i = 0;
+	if (cstr != NULL)
+		while (*(str + i) != '\0')
+		{
+			*(cstr + i) = *(str + i);
+			i++;
+		}
+	return (cstr);
+}
+
