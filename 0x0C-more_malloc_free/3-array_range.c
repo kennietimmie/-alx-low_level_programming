263c7dd9cc025a3716ec965477dd9d1e52af5906
Kehinde Sholadoye <kehinde.sholadoye@yahoo.com>
2023-03-16 21:40:55 +0100

    update 3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -15,7 +15,7 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 
-	arange = malloc(sizeof(int) * (max - min) + 1);
+	arange = malloc(sizeof(int) * ((max - min) + 1));
 
 	if (arange == NULL)
 		return (NULL);
