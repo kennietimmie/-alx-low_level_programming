2d589fafb29d69a231a30da9d4d196a5c5f6d515
Kehinde Sholadoye <kehinde.bridgewateracademy@gmail.com>
2023-03-16 23:04:50 +0100

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,34 +11,37 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i = 0, j, k, s1len = 0, s2len = 0;
+	unsigned int i = 0, st, s1len = 0, s2len = 0;
 	char *str;
 
-	while ((s1 && s1[s1len]) || (s2 && s2[s2len]))
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = ""
+	while (s1[s1len] || s2[s2len])
 	{
-		if (s1 && s1[s1len])
+		if (s1[s1len])
 			s1len++;
-		if (s2 && s2[s2len])
+		if (s2[s2len])
 			s2len++;
 	}
 
 	if (n > s2len)
 		n = s2len;
-
-	str = malloc(sizeof(char) * (s1len + n + 1));
+	st = s1len + n;
+	str = malloc(sizeof(char) * (st + 1));
 
 	if (str == NULL)
 		return (str);
 
 	/* add first string */
-	for (j = 0; s1 && s1[j]; i++, j++)
-		str[i] = s1[j];
-
-	/* add second string */
-	for (k = 0; s2 && s2[k] && k < n; k++, i++)
-		str[i] = s2[k];
-
+	for (i = 0; i < st; i++)
+	{
+		if (i < s1len)
+			str[i] = s1[i];
+		else
+			str[i] = s2[i - s1len];
+	}
 	str[i] = '\0';
 	return (str);
 }
-
