3a8f6e39ed7a2ae0d7ea2b84a977bbe9c7e830ae
Kehinde Sholadoye <kehinde.sholadoye@yahoo.com>
2023-03-16 19:41:00 +0100

    create 1-string_nconcat.c

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * string_nconcat - concatenates two strings.
+ * @s1: first string.
+ * @s2: second string.
+ * @n: number of string to concat
+ *
+ * Return: If success pointer to new space otherwise NULL
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i = 0, j, k, s1len = 0, s2len = 0;
+	char *str;
+	
+	while ((s1 && s1[s1len]) || (s2 && s2[s2len]))
+	{
+		if (s1 && s1[s1len])
+			s1len++;
+		if (s2 && s2[s2len])
+			s2len++;
+	}
+	s1len -= 1;
+	s2len -= 1;
+
+	if (n >= s2len)
+		str = malloc(sizeof(char) * (s1len + s2len + 1));
+	else
+		str = malloc(sizeof(char) * (s1len + n + 1));
+	if (str == NULL)
+		return (str);
+
+	for (j = 0; s1 && s1[j]; i++, j++)
+		str[i] = s1[j];
+	for (k = 0; s2 && s2[k] && k < n; k++, i++)
+		str[i] = s2[k];
+
+	str[i] = '\0';
+	return (str);
+}
+
