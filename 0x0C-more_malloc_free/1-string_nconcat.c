c87c0308694781af9762ba1a97ef15b8823c158a
Kehinde Sholadoye <kehinde.bridgewateracademy@gmail.com>
2023-03-16 23:05:40 +0100

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -17,7 +17,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
-		s2 = ""
+		s2 = "";
 	while (s1[s1len] || s2[s2len])
 	{
 		if (s1[s1len])
