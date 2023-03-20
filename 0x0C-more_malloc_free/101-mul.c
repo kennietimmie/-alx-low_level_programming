472b8df10beb4264c5b3798ea934b37933feb8a0
Kehinde Sholadoye <kehinde.sholadoye@yahoo.com>
2023-03-20 19:27:01 +0100

    fail attempt

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <strings.h>
 
 /**
  * main - multiply to positive numbers
@@ -11,14 +12,16 @@
  */
 int main(int argc, char *argv[])
 {
-	unsigned long a, b;
+	unsigned long l1, l2, lout;
 	int i, j;
+	char * sout;
 
 	if (argc != 3)
 	{
 		printf("Error\n");
 		exit(98);
 	}
+	/* exit if s1 || s2 contains non digit */
 	for (i = 1; i < argc; i++)
 		for (j = 0; argv[i][j] != '\0'; j++)
 			if (argv[i][j] < 48 || argv[i][j] > 57)
@@ -27,10 +30,18 @@ int main(int argc, char *argv[])
 				exit(98);
 			}
 
-	a = atoi(argv[1]);
-	b = atoi(argv[2]);
+	/* exit if a || b is zero */
+	if (atoi(argv[1]) <= 0 || atoi(argv[2]) <= 0)
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
-	if (a == 0 || b == 0)
+	l1 = strlen(argv[1]);
+	l2 = strlen(argv[2]);
+	lout = l1 + l2;
+	sout = malloc(lout + 1);
+	if (sout == NULL)
 	{
 		printf("Error\n");
 		exit(98);
