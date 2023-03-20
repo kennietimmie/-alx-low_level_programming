2e0c449db7e6e46a61b986702399f986e1209d6e
Kehinde Sholadoye <kehinde.sholadoye@yahoo.com>
2023-03-21 00:50:08 +0100

    update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -25,6 +25,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 			free(n_dog->name);
 			free(n_dog->owner);
 			free(n_dog);
+			return (NULL);
 		}
 		n_dog->name = strcpy(n_dog->name, name);
 		n_dog->owner = strcpy(n_dog->owner, owner);
