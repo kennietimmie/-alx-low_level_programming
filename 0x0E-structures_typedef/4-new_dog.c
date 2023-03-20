623e7fc773f839b82d2945cc4d47e278d89f635a
Kehinde Sholadoye <kehinde.sholadoye@yahoo.com>
2023-03-21 00:27:30 +0100

    style guide

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,6 @@
 #include "dog.h"
 #include <stdlib.h>
-#inlcude <strings.h>
+#include <strings.h>
 
 /**
  * new_dog - creates a new dog
@@ -13,7 +13,7 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *n_dog;
-	
+
 	if (n_dog == NULL)
 		return (NULL);
 	if (name != NULL && owner != NULL)
@@ -21,19 +21,19 @@ dog_t *new_dog(char *name, float age, char *owner)
 		n_dog = malloc(sizeof(dog_t));
 		n_dog->name = malloc(sizeof(char) * strlen(name));
 		n_dog->owner = malloc(sizeof(char) * strlen(owner));
-		
+
 		if (n_dog->owner == NULL || n_dog->name == NULL || n_dog == NULL)
 		{
 			free(n_dog->name);
 			free(n_dog->owner);
 			free(n_dog);
 		}
-		
 		n_dog->name = strcpy(n_dog->name, name);
 		n_dog->owner = strcpy(n_dog->owner, owner);
 		n_dog->age = age;
-		
+
 		return (n_dog);
 	}
 	return (NULL);
 }
+
