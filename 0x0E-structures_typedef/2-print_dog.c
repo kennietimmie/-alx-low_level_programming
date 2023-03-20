00554ed6f7cddb0192dfc62d3a9f3606109b7c2b
Kehinde Sholadoye <kehinde.sholadoye@yahoo.com>
2023-03-20 23:46:14 +0100

    update 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,15 +13,15 @@ void print_dog(struct dog *d)
 	if (d != NULL)
 	{
 		if (d->name == NULL)
-			printf("Name: (Nil)\n");
+			printf("Name: (nil)\n");
 		else
 			printf("Name: %s\n", d->name);
 		if (d->age)
-			printf("Age: (Nil)\n");
+			printf("Age: (nil)\n");
 		else
 			printf("age: %f\n", d->age);
 		if (d->owner == NULL)
-			printf("Owner: (Nil)\n");
+			printf("Owner: (nil)\n");
 		else
 			printf("Owner: %s\n", d->owner);
 	}
