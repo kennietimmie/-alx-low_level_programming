a1b250220843e003f0a73a709bef386ebda98f78
Kehinde Sholadoye <kehinde.bridgewateracademy@gmail.com>
2023-03-21 00:57:20 +0100

    Update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,15 +12,18 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *n_dog;
+	dog_t *n_dog
 
 	if (name != NULL && owner != NULL)
 	{
 		n_dog = malloc(sizeof(dog_t));
+		if (n_dog == NULL)
+			return (NULL);
+
 		n_dog->name = malloc(sizeof(char) * (strlen(name) + 1));
 		n_dog->owner = malloc(sizeof(char) * (strlen(owner) + 1));
 
-		if (n_dog->owner == NULL || n_dog->name == NULL || n_dog == NULL)
+		if (n_dog->owner == NULL || n_dog->name == NULL)
 		{
 			free(n_dog->name);
 			free(n_dog->owner);
