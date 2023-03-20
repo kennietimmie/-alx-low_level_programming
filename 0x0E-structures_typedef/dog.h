6947f0e844e315ba2235b57f7602bd7f75e8764f
Kehinde Sholadoye <kehinde.bridgewateracademy@gmail.com>
2023-03-21 00:30:38 +0100

    Create dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,11 +18,12 @@ struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
 
 /**
  * dog_t - Typedef for dog structure
  */
 typedef struct dog dog_t;
+
+dog_t *new_dog(char *name, float age, char *owner);
 
 #endif
