146c3146de039a02fcdb786e7ea825d005bfb235
Kehinde Sholadoye <kehinde.sholadoye@yahoo.com>
2023-03-20 21:00:08 +0100

    create dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - A dog struct
+ * @name: name of dog
+ * @age: dog's age
+ * @owner: dog's owner name
+ *
+ * Description: a struct containing dog properties
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
