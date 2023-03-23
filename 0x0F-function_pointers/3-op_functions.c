a5e3fc3a8b71d3be39f8d51fdf58140cfab18664
kennietimmie <kehinde.sholadoye@yahoo.com>
2023-03-23 07:54:08 +0100

    Create 3-op_functions.c

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,58 @@
+/**
+ * op_add - add two integers
+ * @a: int a
+ * @b: int b
+ *
+ * Return: Always int
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtract two integers
+ * @a: int a
+ * @b: int b
+ *
+ * Return: Always int
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - product of two integers
+ * @a: int a
+ * @b: int b
+ *
+ * Return: Always int
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - division of two integers
+ * @a: int a
+ * @b: int b
+ *
+ * Return: Always int
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - remainder of the division
+ * @a: int a
+ * @b: int b
+ *
+ * Return: Always int
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
