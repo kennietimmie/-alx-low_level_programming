fd1b4663216178a5851dcc418732faf328d34173
kennietimmie <kehinde.sholadoye@yahoo.com>
2023-03-23 08:02:14 +0100

    Create 3-get_func.c

---
+++ 0x0F-function_pointers/3-get_func.c
@@ -0,0 +1,29 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * get_op_func - get function
+ * @s: op type
+ *
+ * Return: Always int
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL},
+	};
+	int i;
+
+	for (i = 0; i < 5; i++)
+	{
+		if (*(ops[i].op) == *s)
+			return (ops[i].f);
+	}
+	return (NULL);
+}
