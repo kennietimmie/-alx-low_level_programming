0f6eaf9295d755a420a7f2f5e9519bddd2c7c2f4
kennietimmie <kehinde.sholadoye@yahoo.com>
2023-03-30 09:15:09 +0100

    Create 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print a - z 10 times
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int i = 10;
+
+	while (i--)
+		print_alphabet();
+}
