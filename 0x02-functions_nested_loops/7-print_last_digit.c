b93a17119253f06e3b14663ed2bccabcc07a3334
kennietimmie <kehinde.sholadoye@yahoo.com>
2023-03-30 11:43:32 +0100

    Create 7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print last number of a number
+ * @n: number
+ *
+ * Return: last number
+ */
+int print_last_digit(int n)
+{
+	n = n < 0 ? n * -1 : n;
+
+	if (n < 9)
+		putchar(n + '0');
+	else
+		putchar((n % 10) + '0');
+	return (n < 9 ? n : n % 10);
+}
