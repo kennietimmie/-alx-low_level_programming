b938c89fc430d9ef7e3f6d16ff45c639aca3441a
kennietimmie <kehinde.sholadoye@yahoo.com>
2023-04-05 01:19:15 +0100

    still working on null

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,7 +16,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (new == NULL)
 		return (NULL);
 
-	if (head && *head)
+	if (head)
 		next = *head;
 
 	new->n = n;
