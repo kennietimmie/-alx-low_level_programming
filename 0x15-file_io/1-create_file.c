ff4d520d3365aeb2a7089264a54597e1fd2b1bc8
kennietimmie <kehinde.sholadoye@yahoo.com>
2023-04-12 20:53:19 +0100

    null check

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,8 +18,9 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	while (*str++)
-		nbyte++;
+	if (text_content)
+		while (*str++)
+			nbyte++;
 
 	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, mode);
 
