476ab4af0ac0393a307f6fa8e8cdf451568c87fb
kennietimmie <kehinde.sholadoye@yahoo.com>
2023-04-12 20:30:43 +0100

    create_file proto

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,8 +6,10 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <stdio.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 
 #endif
