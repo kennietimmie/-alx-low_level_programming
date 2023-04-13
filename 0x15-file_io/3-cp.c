89536bdafe3ac69c5b4d23427c89efa193fd0964
kennietimmie <kehinde.sholadoye@yahoo.com>
2023-04-13 07:25:32 +0100

    error message

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -25,7 +25,7 @@ int main(int argc, char *argv[] )
 
 	if (file_from == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file $%s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 
