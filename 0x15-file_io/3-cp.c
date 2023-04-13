8401926b9517e60153ca3ef7ba9381ebcb467d1c
kennietimmie <kehinde.sholadoye@yahoo.com>
2023-04-13 06:53:14 +0100

    close_file definition

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -11,3 +11,18 @@ int main(int argc __attribute__ ((unused)), char *argv[] __attribute__ ((unused)
 {
 	return (0);
 }
+
+/**
+ * close_file - close a file descriptor
+ * @fd: file descriptor.
+ *
+ * Return: void
+ */
+void close_file(int fd)
+{
+	if (close(fd) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
