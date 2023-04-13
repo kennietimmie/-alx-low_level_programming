b5771cdc46c1e93e5814cc1d7e804d7e25734144
kennietimmie <kehinde.sholadoye@yahoo.com>
2023-04-13 06:57:18 +0100

    use close_file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,8 +7,22 @@
  *
  * Return: Always 0.
  */
-int main(int argc __attribute__ ((unused)), char *argv[] __attribute__ ((unused)))
+int main(int argc, char *argv[] )
 {
+	int file_from, file_to;
+	mode_t mode = S_IRUSR | S_IWUSR | S_IWGRP | S_IRGRP | S_IROTH;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	file_from = open(argv[1], O_RDONLY);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, mode);
+
+	close_file(file_from);
+	close_file(file_to);
 	return (0);
 }
 
