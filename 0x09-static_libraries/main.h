df4401d4ff08ba1747927cecca8d97638b732908
Kehinde Sholadoye <kehinde.s@broadshiftconnect.com>
2023-11-20 11:01:12 +0100

    library

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,5 +1,5 @@
 #ifndef STATIC_LIBRARIES
-#ifdef STATIC_LIBRARIES
+#define STATIC_LIBRARIES
 
 int _putchar(char c);
 int _islower(int c);
@@ -21,3 +21,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+
+#endif
